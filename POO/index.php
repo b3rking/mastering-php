45505c9291d2b17f8d888f4e4cadd93a01947b93
b3rking <ishimweeschylebersy@gmail.com>
2021-08-11 09:15:27 +0200

    methods

--- POO/index.php
+++ POO/index.php
@@ -4,14 +4,23 @@
 
 // echo $date->format('d/m/Y');
 
-declare (strict_types=1);
+// declare (strict_types=1);
 
 class Pont
 {
     public float $longueur = 0;
+    public float $largeur = 0;
+
+    public function getSurface(): float
+    {
+        return $this->longueur * $this->largeur;
+    }
 }
 
 $pont = new Pont;
 $pont->longueur = 235.6;
+$pont->largeur = 254;
 
-var_dump($pont);
+$surface = $pont->largeur * $pont->longueur;
+
+var_dump($surface);
