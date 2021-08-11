76b04d611c146737123416be114e1cb2f4ce2d9d
b3rking <ishimweeschylebersy@gmail.com>
2021-08-11 09:05:21 +0200

    nouveau class

--- POO/index.php
+++ POO/index.php
@@ -1,2 +1,17 @@
 <?php
 
+// $date = new DateTime;
+
+// echo $date->format('d/m/Y');
+
+declare (strict_types=1);
+
+class Pont
+{
+    public float $longueur = 0;
+}
+
+$pont = new Pont;
+$pont->longueur = 235.6;
+
+var_dump($pont);
