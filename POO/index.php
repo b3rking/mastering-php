2dcf5e0ce2221e107aad219f83d37b6fe3e79230
b3rking <ishimweeschylebersy@gmail.com>
2021-08-11 10:04:57 +0200

    encapsulation, immutables, static

--- POO/index.php
+++ POO/index.php
@@ -4,22 +4,51 @@
 
 // echo $date->format('d/m/Y');
 
-// declare (strict_types=1);
+declare (strict_types=1);
 
 class Pont
 {
-    public float $longueur = 0;
-    public float $largeur = 0;
-    public string $unite = 'm²';
+    private float $longueur = 0;
+    private float $largeur = 0;
+    private const UNITE = 'm²';
+
+
+    public static function valideTaille(float $l): bool
+    {
+        if ($l <= 50.0) {
+            trigger_error('longueur trop court', E_USER_ERROR);
+        }
+
+        return true;
+    }
+
+    public function setLongueur(float $l): void
+    {
+        self::valideTaille($l);
+
+        $this->longueur = $l;
+    }
+
+    public function setLargeur(float $l): void
+    {
+        self::valideTaille($l);
+
+        $this->largeur = $l;
+    }
 
     public function getSurface(): string
     {
-        return ($this->longueur * $this->largeur) .$this->unite;
+        return ($this->longueur * $this->largeur) . self::UNITE;
+    }
+
+    public function getLongueur():float
+    {
+        return $this->longueur;
     }
 }
 
 $pont = new Pont;
-$pont->longueur = 235.6;
-$pont->largeur = 254;
+$pont->setLongueur(235.6);
+$pont->setLargeur(254);
 
 var_dump($pont->getSurface());
