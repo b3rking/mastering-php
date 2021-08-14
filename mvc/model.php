987f4bbad39dfcb9bc29e0e6d74b200f9642db86
b3rking <ishimweeschylebersy@gmail.com>
2021-08-14 19:36:33 +0200

    added new functions

--- mvc/model.php
+++ mvc/model.php
@@ -1,17 +1,46 @@
 <?php
-function getPosts()
-{
+
+function connexion() {
 	try
 	{
 	    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+		return $bdd;
 	}
 	catch(Exception $e)
 	{
-	    die('Erreur : '.$e->getMessage());
+	    return die('Erreur : '.$e->getMessage());
 	}
+}
+
+function getPosts()
+{
+	$bdd = connexion();
 
 	$req = $bdd->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM billets ORDER BY creation_date DESC LIMIT 0, 5');
 
 	return $req;
 }
+
+function getPost($postId)
+{
+	$db = connexion();
+
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+    $req->execute(array($postId));
+    $post = $req->fetch();
+
+    return $post;
+}
+
+function getComments($postId)
+{
+    $db = connexion();
+
+    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+    $comments->execute(array($postId));
+
+    return $comments;
+}
+
+
 ?>
