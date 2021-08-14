28552a752158ddc856c0976b48c35b53ed0d2017
b3rking <ishimweeschylebersy@gmail.com>
2021-08-14 11:26:49 +0200

    some work

--- mvc/indexView.php
+++ mvc/indexView.php
@@ -17,16 +17,14 @@
         ?>
         <div class="news">
             <h3>
-                <?php echo htmlspecialchars($donnees['titre']); ?>
-                <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+                <?= htmlspecialchars($donnees['titre']); ?>
+                <em>le <?= $donnees['date_creation_fr']; ?></em>
             </h3>
             
             <p>
-            <?php
-            echo nl2br(htmlspecialchars($donnees['contenu']));
-            ?>
-            <br />
-            <em><a href="#">Commentaires</a></em>
+                <?= nl2br(htmlspecialchars($donnees['contenu'])); ?>
+                <br />
+                <em><a href="#">Commentaires</a></em>
             </p>
         </div>
         <?php
