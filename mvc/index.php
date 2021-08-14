d26b8476f114a945f74d4555e67e5eca5d5d18e2
b3rking <ishimweeschylebersy@gmail.com>
2021-08-14 11:23:23 +0200

    created models

--- mvc/index.php
+++ mvc/index.php
@@ -1,15 +1,8 @@
 <?php
 // Récupération des données
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+require_once('./model.php');
 
-$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+getBillets();
 
 require('./showAccueil.php');
 ?>
